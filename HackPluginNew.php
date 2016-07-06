b0d8605071840b9f912fba991dc32ecffb1dcd06
Jan Pietrzyk <jp@shopware.com>
2016-07-06 14:23:19 +0200

    initial commit

---
+++ HackPluginNew.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+
+namespace HackPluginNew;
+
+
+class HackPluginNew extends \Shopware\Components\Plugin
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Front_RouteStartup' => 'onRouteStartup'
+        ];
+    }
+
+    public function onRouteStartup(\Enlight_Controller_EventArgs $args)
+    {
+        die("A rolling stone gathers no moss");
+    }
+}
